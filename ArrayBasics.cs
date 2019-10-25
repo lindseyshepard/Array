781ad21a22c679e5c23814ecf50c6c54eb4900d3
lindseyshepard <55165590+lindseyshepard@users.noreply.github.com>
2019-10-25 12:21:04 -0400

    Update
    Uploaded files for the array program. This is just practice because it was has a while since I have done anything with arrays.

---
+++ ArrayBasics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Array
+{
+    class ArrayBasics
+    {
+
+
+
+        //passing array as parameter and return values for a method
+        public int[] ReadDate()
+        {
+            Console.WriteLine("How many elements?");
+            string reply = Console.ReadLine();
+            int numElements = int.Parse(reply);
+
+            int[] data = new int[numElements];
+            for (int i = 0; i < numElements; i++)
+            {
+                Console.WriteLine($"Enter data for element {i}");
+                reply = Console.ReadLine();
+                int elementData = int.Parse(reply);
+                data[i] = elementData;
+            }
+            return data;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelperMethods helperMethods = new HelperMethods();
+
+
+            ArrayBasics arrayBasics = new ArrayBasics();
+            arrayBasics.ReadDate();
+
+        }
+    }
+}
